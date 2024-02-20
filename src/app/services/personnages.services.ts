fdcbcbdb316f1257bc7e596f5da4a5f0886d8619
Lhukas <58892260+Lhukas@users.noreply.github.com>
2024-02-20 12:06:34 +0100

    git update

--- src/app/services/personnages.services.ts
+++ src/app/services/personnages.services.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { CharactersQuery, CharactersQueryVariables } from './generated/graphql'; // Assurez-vous d'avoir le bon chemin
+import {Character, CharactersQuery, CharactersQueryVariables, CharactersDocument} from '../generated/graphql';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +12,21 @@ export class CharacterService {
   constructor(private apollo: Apollo) {}
 
   getCharacters(): Observable<Character[]> {
+
+
+    // @ts-ignore
     return this.apollo
       .query<CharactersQuery, CharactersQueryVariables>({
         query: CharactersDocument,
       })
       .pipe(
+        // @ts-ignore
         map(result => result.data.characters.results.map(character => ({
+          // @ts-ignore
           id: character.id,
+          // @ts-ignore
           name: character.name,
+          // @ts-ignore
           image: character.image,
         })))
       );
