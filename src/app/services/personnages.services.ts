a8f8ea679c67488ee3591af6bc797f9fa5c9c333
Lhukas <58892260+Lhukas@users.noreply.github.com>
2024-02-20 14:14:41 +0100

    git update - clean version

--- src/app/services/personnages.services.ts
+++ src/app/services/personnages.services.ts
@@ -1,34 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import {Character, CharactersQuery, CharactersQueryVariables, CharactersDocument} from '../generated/graphql';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
 
-  constructor(private apollo: Apollo) {}
+  private apiUrl = 'https://rickandmortyapi.com/graphql';
 
-  getCharacters(): Observable<Character[]> {
+  constructor(private http: HttpClient) {}
 
+  getCharacters() {
+    const query = `
+      query {
+        characters {
+          results {
+            id
+            name
+            image
+          }
+        }
+      }
+    `;
 
-    // @ts-ignore
-    return this.apollo
-      .query<CharactersQuery, CharactersQueryVariables>({
-        query: CharactersDocument,
-      })
-      .pipe(
-        // @ts-ignore
-        map(result => result.data.characters.results.map(character => ({
-          // @ts-ignore
-          id: character.id,
-          // @ts-ignore
-          name: character.name,
-          // @ts-ignore
-          image: character.image,
-        })))
-      );
+    return this.http.post(this.apiUrl, { query });
   }
 }
